be7cd0b1036a36f14ba3cfb69ad1d48514a1fe2e
Стас Кирей <staskirey1@gmail.com>
2025-05-12 14:12:27 +0300

    фінальний результат

--- REF1/Inventory.cs
+++ REF1/Inventory.cs
@@ -1,15 +1,13 @@
 ﻿namespace REF1
 {
-    public class Inventory
+    class Inventory
     {
         private List<string> products = new List<string> { "Laptop", "Mouse", "Keyboard" };
 
-        public void CheckStock(string product)
+        public bool CheckStock(string product)
         {
-            if (products.Contains(product))
-                Console.WriteLine($"{product} is in stock.");
-            else
-                Console.WriteLine($"{product} is out of stock.");
+            return products.Contains(product);
         }
     }
+
 }
--- REF1/User.cs
+++ REF1/User.cs
@@ -1,20 +1,10 @@
 ﻿
 namespace REF1
 {
-    public class User
+    class User
     {
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-
-        public void SaveToDatabase()
-        {
-            Console.WriteLine("Saving user to database...");
-        }
-
-        public void SendEmail(string message)
-        {
-            Console.WriteLine($"Sending email to {Email}: {message}");
-        }
     }
 }
