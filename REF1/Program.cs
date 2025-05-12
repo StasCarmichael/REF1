ac695542bad23925144b233cf6b3832adfe848fc
stask <stask@DESKTOP-LCRV3PH>
2025-05-12 13:32:22 +0300

    Добавьте файлы проекта.

--- REF1/Program.cs
+++ REF1/Program.cs
@@ -4,7 +4,7 @@
     class Program
     {
         static void Main()
-        {
+    {
             IUserRepository userRepository = new UserRepository();
             IEmailService emailService = new EmailService();
 
@@ -15,13 +15,13 @@
             IPaymentProcessor payment = new CreditCardPayment();
             InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
             Order order = new Order(payment, invoiceGenerator)
-            {
+        {
                 Id = 1,
                 Products = new List<string> { "Laptop", "Mouse" },
                 TotalAmount = 1500
             };
             order.ProcessOrder();
-        }
     }
+}
 
 }
